1190dbd8075d7c62e5e20c2806f4b01e47d191dd
Loi Vo <loitanvo@gmail.com>
2019-08-28 17:09:31 -0600

    errors call back

--- src/index.js
+++ src/index.js
@@ -8,21 +8,24 @@ class App extends React.Component{
     constructor (props) {
         super(props);
 
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => this.setState({ lat: position.coords.latitude }),
-            (err) => console.log(err)
+            (err) => this.setState({ errorMessage: err.message})
         )
     };
     
 
     render() {
-     
+     if (this.state.errorMessage && !this.state.lat) {
+        return <div> error: {this.state.errorMessage}</div>
+     }
     
-        return  <div>
-            Latitude: {this.state.lat}
-           </div>
+     if (!this.state.errorMessage && this.state.lat){
+         return <div> Lattitude: {this.state.lat}</div>
+     }
+     return <div>loading this is a test</div>
     }
 
 };
